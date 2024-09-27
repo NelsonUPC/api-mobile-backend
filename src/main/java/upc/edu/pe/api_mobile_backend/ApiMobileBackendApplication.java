935ce892e7954f21eea71789894af9e651496db9
NelsonUPC <164695009+NelsonUPC@users.noreply.github.com>
2024-09-26 23:19:23 -0500

    feat: project structure added

---
+++ src/main/java/upc/edu/pe/api_mobile_backend/ApiMobileBackendApplication.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.api_mobile_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMobileBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMobileBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/upc/edu/pe/api_mobile_backend/ApiMobileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.api_mobile_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMobileBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
